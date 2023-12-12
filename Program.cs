11b25d82ddccef718cb517f4313878dccd86edf2
Evgenii Uvarov <ev9en1149@gmail.com>
2023-12-12 11:07:21 +0300

    add file

--- Program.cs
+++ Program.cs
@@ -5,12 +5,20 @@ Clear();
 WriteLine("Write number: ");
 int numA = int.Parse(ReadLine());
 
-if (Math.Abs(numA) < 99)
+if(Math.Abs(numA) < 1 || Math.Abs(numA) > 7 )
 {
-   WriteLine("Введено не трёх значное число: ");
+   WriteLine("такого дня нет");
    return;
 }
-WriteLine(numA%100%10);
+if(numA >= 1 & numA < 6) WriteLine("Это будний Day");
+else WriteLine("Это выходной Day");
 
 
 
+// if(numA == 1) WriteLine("Monday");
+// if(numA == 2) WriteLine("Tuesday");
+// if(numA == 3) WriteLine("Wednesday");
+// if(numA == 4) WriteLine("Thursday");
+// if(numA == 5) WriteLine("Friday");
+// if(numA == 6) WriteLine("Saturday");
+// if(numA == 7) WriteLine("Sunday");
