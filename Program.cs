37c3d54dd8aa61c07dc3efb01458b0fe7d5db94b
Evgenii Uvarov <ev9en1149@gmail.com>
2023-12-12 11:16:59 +0300

    Holliday

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ Clear();
 WriteLine("Write number: ");
 int numA = int.Parse(ReadLine());
 
-if(Math.Abs(numA) < 1 || Math.Abs(numA) > 7 )
+if(numA < 1 || numA > 7 )
 {
    WriteLine("такого дня нет");
    return;
@@ -13,12 +13,3 @@ if(Math.Abs(numA) < 1 || Math.Abs(numA) > 7 )
 if(numA >= 1 & numA < 6) WriteLine("Это будний Day");
 else WriteLine("Это выходной Day");
 
-
-
-// if(numA == 1) WriteLine("Monday");
-// if(numA == 2) WriteLine("Tuesday");
-// if(numA == 3) WriteLine("Wednesday");
-// if(numA == 4) WriteLine("Thursday");
-// if(numA == 5) WriteLine("Friday");
-// if(numA == 6) WriteLine("Saturday");
-// if(numA == 7) WriteLine("Sunday");
